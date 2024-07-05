d617a7c18e485a88ccb36cd0cc8c9cffc871d6a4
shubhamvaishnav18 <shubhamvaishnav332@gmail.com>
2024-07-05 21:36:48 +0530

    Update files

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -20,7 +19,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
 
 main()
     .then(() => {
@@ -31,7 +31,7 @@ main()
     });
 
 async function main() {
-    await mongoose.connect(dbUrl);
+    await mongoose.connect(MONGO_URL);
 }
 
 app.set("view engine", "ejs");
@@ -41,20 +41,8 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create({
-    mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
-    touchAfter: 24 * 3600,
-});
-
-store.on("error", () => {
-    console.log("ERROR IN MONGO SESSION STORE", err);
-});
-
 const sessionOptions = {
-    store,
+    //store,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
@@ -64,10 +52,6 @@ const sessionOptions = {
         httpOnly: true,
     },   
 };
-
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
 
 app.use(session(sessionOptions));
 app.use(flash());
@@ -86,15 +70,6 @@ app.use((req, res, next) => {
     next(); 
 });
 
-// app.get("/demouser", async (req, res) => {
-//     let fakeUser = new User({
-//         email: "student@gmail.com",
-//         username: "delta-student",
-//     });
-//     let registeredUser = await User.register(fakeUser, "helloworld");
-//     res.send(registeredUser);
-// });
-
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
@@ -106,7 +81,6 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "Something went wrong!"} = err;
     res.status(statusCode).render("error.ejs", {message});
-    //res.status(statusCode).send(message);
 });
 
 app.listen(8080, () => {
